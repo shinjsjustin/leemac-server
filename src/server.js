3f8f099e3985b967b0f5e6c250b85a5192875bb1
shinjsjustin <shinjsjustin@gmail.com>
2024-11-14 15:54:27 -0800

    revert to http

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const https = require('https');
+// const fs = require('fs');
+// const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
@@ -15,19 +15,19 @@ dotenv.config();
 const cors = require('cors');
 const app = express();
 
-const options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/leemac.co/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/leemac.co/fullchain.pem'),
-};
+// const options = {
+//     key: fs.readFileSync('/etc/letsencrypt/live/leemac.co/privkey.pem'),
+//     cert: fs.readFileSync('/etc/letsencrypt/live/leemac.co/fullchain.pem'),
+// };
 
 // Redirect HTTP to HTTPS
-const http = require('http');
-http.createServer((req, res) => {
-    res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
-    res.end();
-}).listen(80, () => {
-    console.log('HTTP server redirecting to HTTPS on port 80');
-});
+// const http = require('http');
+// http.createServer((req, res) => {
+//     res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+//     res.end();
+// }).listen(80, () => {
+//     console.log('HTTP server redirecting to HTTPS on port 80');
+// });
 
 const isAuth = require('./middleware/isAuth');
 
@@ -44,9 +44,9 @@ app.use('/api/quote', quoteRoutes);
 // app.use('/sheet', sheetRoutes);
 
 // Serve HTTPS
-https.createServer(options, app).listen(443, () => {
-    console.log('HTTPS server running on port 443');
-});
+// https.createServer(options, app).listen(443, () => {
+//     console.log('HTTPS server running on port 443');
+// });
 
 // Catch-all handler for React
 app.get('*', (req, res) => {
@@ -60,4 +60,9 @@ app.get('/', (req, res) => {
 
 app.get('/test', (req, res) => {
     res.status(201).json({ message: 'Ur bumd' });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
 });
